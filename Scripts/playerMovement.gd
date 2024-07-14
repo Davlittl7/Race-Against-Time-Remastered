c95e752771355eb07c092998ae7c285a7aea88e0
Davlittl7 <davontelittleton77@gmail.com>
2024-07-13 22:31:17 -0400

    Added diff animations for player

--- Scripts/playerMovement.gd
+++ Scripts/playerMovement.gd
@@ -16,13 +16,22 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.animation = "Jump"
+	
+	#Handles direction of player sprite depending on if going left or right 
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	else: 
+		$AnimatedSprite2D.flip_h = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.animation = "Run"
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.animation = "Idle"
 
 	move_and_slide()
